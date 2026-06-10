c7520c7e40c86dd43dae8cdeb5866dbab9c3e8d3
jkaliski <jjkaliski@gmail.com>
2026-06-10 05:32:23 +0200

    Add Sanity Dashboard compatibility

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,9 +1,19 @@
 'use client'
 
 import {NextStudio} from 'next-sanity/studio'
+import {preloadModule} from 'react-dom'
 
 import config from '../../../../sanity.config'
 
+const dashboardBridgeScript = 'https://core.sanity-cdn.com/bridge.js'
+
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  preloadModule(dashboardBridgeScript, {as: 'script'})
+
+  return (
+    <>
+      <script src={dashboardBridgeScript} async type="module" />
+      <NextStudio config={config} />
+    </>
+  )
 }
